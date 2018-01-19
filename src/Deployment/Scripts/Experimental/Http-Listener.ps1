e67843bebda857f67ebbdb84513da89e3f863174
SN\plastic <varga.yosh@gmail.com>
2018-01-19 10:00:16 +0100

    ADD experimental http listener script

---
+++ src/Deployment/Scripts/Experimental/Http-Listener.ps1
@@ -0,0 +1,38 @@
+# Use the following commands to bind/unbind SSL cert
+# netsh http add sslcert ipport=0.0.0.0:443 certhash=3badca4f8d38a85269085aba598f0a8a51f057ae "appid={00112233-4455-6677-8899-AABBCCDDEEFF}"
+# netsh http delete sslcert ipport=0.0.0.0:443 
+$HttpListener = New-Object System.Net.HttpListener
+$HttpListener.Prefixes.Add("http://+:80/")
+$HttpListener.Prefixes.Add("https://+:443/")
+$HttpListener.Start()
+While ($HttpListener.IsListening) {
+	$Result = 0
+    $HttpContext = $HttpListener.GetContext()
+    $HttpRequest = $HttpContext.Request
+    $RequestUrl = $HttpRequest.Url.OriginalString
+    Write-Host "$RequestUrl"
+    if($HttpRequest.HasEntityBody) {
+      $Reader = New-Object System.IO.StreamReader($HttpRequest.InputStream)
+      # $json = $Reader.ReadToEnd() | ConvertFrom-Json 
+	  $json = $Reader.ReadToEnd() 
+
+	  Write-Host Param: $json
+	  if($json -match '.*mode=(?<Mode>.+)')
+			{
+				 $Mode = $Matches.Mode
+				 Write-Host mode: $Mode
+				 & ../Run.ps1 -mode $Mode
+				 $Result = $LASTEXITCODE
+				
+			}
+    }
+    $HttpResponse = $HttpContext.Response
+    $HttpResponse.Headers.Add("Content-Type","application/json")
+    $HttpResponse.StatusCode = 200
+    $ResponseBuffer = [System.Text.Encoding]::UTF8.GetBytes("{ExitCode:$Result}")
+    $HttpResponse.ContentLength64 = $ResponseBuffer.Length
+    $HttpResponse.OutputStream.Write($ResponseBuffer,0,$ResponseBuffer.Length)
+    $HttpResponse.Close()
+    Write-Output "" # Newline
+}
+$HttpListener.Stop()
