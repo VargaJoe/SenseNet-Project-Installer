bae209e39a572569f3dde1057221fd5a29c0bd5e
vargajoe <varga.yosh@gmail.com>
2019-10-12 13:36:21 +0200

    create artifact step downloads vswhere if needed

--- src/Deployment/Scripts/Dev/Create-Artifact.ps1
+++ src/Deployment/Scripts/Dev/Create-Artifact.ps1
@@ -14,12 +14,33 @@ $LASTEXITCODE = 0
 if (!$msbuildPath) {
 	$vsWhereFilePath = "..\Tools\vswhere\vswhere.exe"
 	Write-Verbose "The msbuild.exe path is missing, we try to locate it with $vsWhereFilePath"
-	if (Test-Path $vsWhereFilePath) {
-		$vsWhere = "..\Tools\vswhere\vswhere.exe"
-		$msbuildPath = & "$vsWhere" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
-	} else {
-		Write-Verbose "$vsWhereFilePath is missing too, please copy this file to the tools folder or fill out msbuild path in the settings"
+	if (!(Test-Path $vsWhereFilePath)) {
+		Write-Verbose "$vsWhereFilePath is missing too, we will try to retrieve it from github..."
+		$vswhereSourceJsonUrl = "https://api.github.com/repos/Microsoft/vswhere/releases/latest"
+		Write-Verbose "Get update info from $vswhereSourceJsonUrl"
+		try {
+			# Get latest release information
+			$vswhereSourceJson = (Invoke-WebRequest $vswhereSourceJsonUrl | ConvertFrom-Json)
+
+			# technical Debt: array should be filtered
+			$latestObj = $vswhereSourceJson.assets[0] #| Where-Object -FilterScript { $_.Naem -eq "vswhere.exe" }			
+			$vswhereFileUrl = $latestObj.browser_download_url
+			Write-Verbose "Latest version of vswhere found at $vswhereFileUrl"
+
+			# Download latest version of vswhere
+			Write-Verbose "Downloading file to $vsWhereFilePath"
+			$WebClient = New-Object System.Net.WebClient
+			$WebClient.DownloadFile("$vswhereFileUrl","$vsWhereFilePath")	
+		}
+		catch [exception] {
+			Write-Verbose "Retrieve a version of $vsWhereFilePath is failed, please check the error message below, or set either msbuild.exe in settings or copy vswhere.exe to the appropriate folder"
+			Write-Host $_.Exception
+			exit 1
+		}
 	}
+	# Get msbuild path with the help of vswhere
+	$msbuildPath = & "$vsWhereFilePath" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
+	Write-Verbose "Msbuild path identified as $msbuildPath"
 }
 
 Write-Verbose "================================================"
